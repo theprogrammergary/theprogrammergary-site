8d01d9ba2259aff06f1b319d0f1620466bf59c53
theprogrammergary <theprogrammergary@gmail.com>
2024-09-29 15:33:56 -0400

    switch to static

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Home = () => {
+  return (
+<h1 className="bg-background-start text-xl">
+	Welcome to Next.js
+</h1>
+  );
+};
+
+export default Home;
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-<h1 className="bg-background-start text-xl">
-	Welcome to next.js
-</h1>
-  );
-}
